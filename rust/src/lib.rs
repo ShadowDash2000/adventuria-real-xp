5bf4e03b527c611694f08b98b48669c05aa8e438
shadod <scout0501@gmail.com>
2026-01-10 20:33:13 +0300

    feat(lib): added godot_tokio

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,9 +1,43 @@
 mod movable;
 mod player;
 
+use godot::classes::Engine;
+use godot::global::godot_warn;
+use godot::init::InitLevel;
+use godot::obj::{NewAlloc, Singleton};
 use godot::prelude::{ExtensionLibrary, gdextension};
+use godot_tokio::AsyncRuntime;
 
 struct RustExtension;
 
 #[gdextension]
-unsafe impl ExtensionLibrary for RustExtension {}
+unsafe impl ExtensionLibrary for RustExtension {
+    fn on_level_init(level: InitLevel) {
+        match level {
+            InitLevel::Scene => {
+                let mut engine = Engine::singleton();
+                engine.register_singleton(AsyncRuntime::SINGLETON, &AsyncRuntime::new_alloc());
+            }
+            _ => (),
+        }
+    }
+
+    fn on_level_deinit(level: InitLevel) {
+        match level {
+            InitLevel::Scene => {
+                let mut engine = Engine::singleton();
+
+                if let Some(async_singleton) = engine.get_singleton(AsyncRuntime::SINGLETON) {
+                    engine.unregister_singleton(AsyncRuntime::SINGLETON);
+                    async_singleton.free();
+                } else {
+                    godot_warn!(
+                        "Failed to find & free singleton -> {}",
+                        AsyncRuntime::SINGLETON
+                    );
+                }
+            }
+            _ => (),
+        }
+    }
+}
